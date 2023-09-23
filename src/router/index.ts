7be6f89b291af62d30b94641a1f2022fee29af0f
burak48 <buraksaglam3@gmail.com>
2023-09-24 01:57:24 +0300

    tailwindcss added

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EstateDashboard from '@/components/EstateDashboard.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'EstateDashboard',
+      component: EstateDashboard,
     },
     {
       path: '/about',
