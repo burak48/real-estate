0f341e4d29e36ee6afa88ced5828f6079fc342a8
burak48 <buraksaglam3@gmail.com>
2023-09-24 12:31:16 +0300

    Header component added

--- src/components/EstateDashboard.vue
+++ src/components/EstateDashboard.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+  <div class="min-h-screen flex flex-col md:flex-row bg-gray-100">
+    <Header />
+
+    <!-- <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10"> -->
+    <div class="flex-1 p-4">
       <p class="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
       <p class="text-gray-500 text-lg">Vue and Tailwind CSS in action</p>
     </div>
@@ -8,14 +11,17 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { fetchAppointments } from '@/api/index';
+import { ref, onMounted } from 'vue'
+import { fetchAppointments } from '@/api/index'
+import Header from '@/components/EstateHeader.vue'
 
-const appointments = ref([]);
+const appointments = ref([])
 
 onMounted(async () => {
-  appointments.value = await fetchAppointments();
-});
+  appointments.value = await fetchAppointments()
+})
 
 console.log(appointments)
 </script>
+
+<style scoped></style>
---
+++ src/components/EstateHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="bg-blue-950 p-4 text-white">
+    <ul class="space-y-2">
+      <li>
+        <router-link to="/" class="block py-2 px-4 hover:bg-blue-600">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/appointments" class="block py-2 px-4 hover:bg-blue-600"
+          >Appointments</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/create-appointment" class="block py-2 px-4 hover:bg-blue-600"
+          >Create Appointment</router-link
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
