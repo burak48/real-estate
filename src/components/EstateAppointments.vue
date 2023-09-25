614cdf125d42d42a5d04aa091b5103f17a6749a6
burak48 <buraksaglam3@gmail.com>
2023-09-26 00:37:38 +0300

    FontAwesome added & Sorting Column improvement

--- src/components/EstateAppointments.vue
+++ src/components/EstateAppointments.vue
@@ -2,32 +2,37 @@
   <div class="bg-white p-4 rounded shadow">
     <h2 class="text-2xl font-semibold mb-4">Appointments</h2>
 
-    <template v-if="formattedAppointments.length > 0">
-      <table class="table-auto w-full">
-        <thead>
-          <tr>
-            <th class="px-4 py-2">ID</th>
-            <th class="px-4 py-2" @click="sortAppointments('appointment_date')">Date</th>
-            <th class="px-4 py-2">Postcode</th>
-            <th class="px-4 py-2">Contact Name</th>
-            <th class="px-4 py-2">Agent Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(appointment, index) in formattedAppointments" :key="index">
-            <td class="px-4 py-2">{{ appointment.fields.appointment_id }}</td>
-            <td class="px-4 py-2">{{ appointment.fields.appointment_date }}</td>
-            <td class="px-4 py-2">{{ appointment.fields.appointment_postcode }}</td>
-            <td class="px-4 py-2">{{ appointment.fields.contact_name || 'N/A' }}</td>
-            <td class="px-4 py-2">{{ appointment.fields.agent_name || 'N/A' }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </template>
-
-    <template v-else>
-      <p class="text-center">Loading data...</p>
-    </template>
+    <div class="overflow-x-auto">
+      <template v-if="formattedAppointments.length > 0">
+        <table class="table-auto w-full">
+          <thead>
+            <tr>
+              <th class="px-4 py-2">ID</th>
+              <th class="px-4 py-2" @click="sortAppointments('appointment_date')">
+                Date
+                <font-awesome-icon icon="fa-solid fa-chevron-up" v-if="sortOrder === 'asc'" />
+                <font-awesome-icon icon="fa-solid fa-chevron-down" v-else />
+              </th>
+              <th class="px-4 py-2">Postcode</th>
+              <th class="px-4 py-2">Contact Name</th>
+              <th class="px-4 py-2">Agent Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(appointment, index) in formattedAppointments" :key="index">
+              <td class="px-4 py-2">{{ appointment.fields.appointment_id }}</td>
+              <td class="px-4 py-2">{{ appointment.fields.appointment_date }}</td>
+              <td class="px-4 py-2">{{ appointment.fields.appointment_postcode }}</td>
+              <td class="px-4 py-2">{{ appointment.fields.contact_name || 'N/A' }}</td>
+              <td class="px-4 py-2">{{ appointment.fields.agent_name || 'N/A' }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </template>
+      <template v-else>
+        <p class="text-center">Loading...</p>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -37,6 +42,7 @@ import { fetchAppointments } from '@/api/index'
 
 const appointments = ref([])
 const sortBy = ref('appointment_date')
+const sortOrder = ref('asc') // Default sorting order
 
 onMounted(async () => {
   const fetchedAppointments = await fetchAppointments()
@@ -49,11 +55,17 @@ const formattedAppointments = computed(() => {
 })
 
 const sortAppointments = (columnName: string) => {
-  sortBy.value = columnName
-  formattedAppointments.value.sort((a, b) => {
-    const dateA = new Date(a.fields[columnName])
-    const dateB = new Date(b.fields[columnName])
-    return dateA - dateB
-  })
+  if (sortBy.value === columnName) {
+    sortOrder.value = sortOrder.value === 'asc' ? 'desc' : 'asc'
+    formattedAppointments.value.reverse()
+  } else {
+    sortBy.value = columnName
+    sortOrder.value = 'asc'
+    formattedAppointments.value.sort((a, b) => {
+      const dateA = new Date(a.fields[columnName])
+      const dateB = new Date(b.fields[columnName])
+      return sortOrder.value === 'asc' ? dateA - dateB : dateB - dateA
+    })
+  }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faChevronUp)
+library.add(faChevronDown)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
